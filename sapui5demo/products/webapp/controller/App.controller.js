0ac30a328a20fdb5d816b483492cdd752022c503
Abhay Singh <abhaybsingh19w33@gmail.com>
2024-08-23 07:58:53 +0000

    UI5 free style app but no output

---
+++ sapui5demo/products/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("ns.products.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
